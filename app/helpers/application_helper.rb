664823b54de8aedafcd440a3ce33254e8c5114fb
EC2 Default User <ec2-user@ip-172-31-18-0.us-east-2.compute.internal>
2019-01-24 20:28:21 +0000

    twitter card meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+      
+    def get_twitter_card_info(invite)
+        twitter_card = {}
+        if page
+            twitter_card[:url] = 'https://meshi-go.herokuapp.com/<%= invite.user.username %>/invites/<%= invite.id %>/messages/new'
+            twitter_card[:title] = '<%= invite.user.username %>の投稿'
+            twitter_card[:description] = '<%= invite.content %>'
+            twitter_card[:image] = 'https://s3.amazonaws.com/meshi-image/<%= invite.user.username %>/<%= invite.id %>.png'
+        else
+            twitter_card[:url] = 'X'
+            twitter_card[:title] = 'X'
+            twitter_card[:description] = 'X'
+            twitter_card[:image] = 'X'
+        end
+            twitter_card[:site] = '@dtb6464'
+            twitter_card
+    end 
 end
