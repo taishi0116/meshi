cb28a4f084b086151e6d2b1b88595383d635a5b1
EC2 Default User <ec2-user@ip-172-31-18-0.us-east-2.compute.internal>
2019-01-24 16:37:54 +0000

    precompile false

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module MESHI
     config.i18n.default_locale = :ja
     
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
