777e804a176b5c5b6502113fc6a519127b23ffef
EC2 Default User <ec2-user@ip-172-31-18-0.us-east-2.compute.internal>
2019-01-24 21:12:18 +0000

    tw card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
     def get_twitter_card_info(invite)
         twitter_card = {}
         if invite
-            twitter_card[:url] = 'https://meshi-go.herokuapp.com/<%= invite.user.username %>/invites/<%= invite.id %>/messages/new'
-            twitter_card[:title] = '<%= invite.user.username %>の投稿'
-            twitter_card[:description] = '<%= invite.content %>'
-            twitter_card[:image] = 'https://s3.amazonaws.com/meshi-image/<%= invite.user.username %>/<%= invite.id %>.png'
+            twitter_card[:url] = 'https://meshi-go.herokuapp.com/#{invite.user.username}/invites/#{invite.id}/messages/new'
+            twitter_card[:title] = '#{invite.user.username}の投稿'
+            twitter_card[:description] = '#{invite.content}'
+            twitter_card[:image] = 'https://s3.amazonaws.com/meshi-image/#{invite.user.username}/#{invite.id}.png'
         else
             twitter_card[:url] = 'X'
             twitter_card[:title] = 'X'
